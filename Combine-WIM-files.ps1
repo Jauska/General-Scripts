05255a9356e8c2dc2720f6b64aa452a6017403b8
Niko Nevalainen <niko@nevalainen.me>
2024-04-01 22:51:15 +0300

    Initial-Commit

---
+++ Combine-WIM-files.ps1
@@ -0,0 +1,32 @@
+$location = Get-Location
+$ParentLocation =  Split-Path -Path $location -Parent
+$resultFilename = "install.wim"
+$ResultFilePath = $ParentLocation+"\"+$resultFilename
+Write-Progress -Activity "Reading WIM files"
+$Wims = Get-ChildItem "*.wim"
+$endresult = @()
+ForEach ($WimFile in $Wims) {
+    $ImageFileInfos = $null
+    $ImageFileInfos = Get-WindowsImage -ImagePath $WimFile.FullName
+    $ExpandedWIMinfo = @() 
+    ForEach ($ImageInfo in $ImageFileInfos) {
+        $row = [pscustomobject]@{
+            WimFilePath = $WimFile.FullName
+            WimIndex = $ImageInfo.ImageIndex
+            ImageName = $ImageInfo.ImageName
+            ImageDescription = $ImageInfo.ImageDescription
+        }
+        $ExpandedWIMinfo += $row
+    }
+    $endresult += $ExpandedWIMinfo
+}
+#$endresult | Out-HtmlView
+Write-Progress -Activity "Wim files read. Starting combine process."
+$imagecount = $endresult.Count
+$index = 0
+foreach ($InstallImage in $endresult) {
+    $index = $index + 1
+    Write-Progress -PercentComplete ($index/$imagecount) -Activity "Working on image $index of $imagecount"
+    Start-Sleep -Milliseconds 2000
+    Export-WindowsImage -SourceImagePath $InstallImage.WimFilePath -SourceIndex $InstallImage.ImageIndex -DestinationImagePath $ResultFilePath -DestinationName $InstallImage.ImageName
+}
