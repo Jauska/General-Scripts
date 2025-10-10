de47502f0734037c9d10bfc2c66e2c637a3bcee3
Jauska <jauska00@gmail.com>
2025-10-10 21:22:35 +0300

    Update NamedLocation IP script

---
+++ Update-NamedLocation-IP.ps1
@@ -0,0 +1,23 @@
+#Requires -Modules Microsoft.Graph.Authentication; Microsoft.Graph.Identity.SignIns -PSEdition Desktop -Version 5.1
+
+$TenantId = ""
+$ClientId = ""
+$CertificateThumbprint = ""
+$DDNS_named_locationname = ""
+
+Connect-MgGraph -TenantId:$TenantId -ClientId:$ClientId -CertificateThumbprint:$CertificateThumbprint 
+
+$WantedNamedLocation = Get-MgIdentityConditionalAccessNamedLocation -Filter:"(DisplayName eq '$DDNS_named_locationname')"
+$LocalPublicIP = (curl -s https://ip.hetzner.com)
+$params = @{
+	"@odata.type" = "#microsoft.graph.ipNamedLocation"
+	DisplayName = "$DDNS_named_locationname"
+    isTrusted = $true
+    }
+	$IpRanges = @{}
+    $params.Add("IpRanges",@())
+    $IpRanges.add("@odata.type", "#microsoft.graph.iPv4CidrRange")
+    $IpRanges.add("CidrAddress", "$LocalPublicIP/32")
+    $params.IpRanges += $IpRanges
+Update-MgIdentityConditionalAccessNamedLocation -NamedLocationId $WantedNamedLocation.Id -BodyParameter $params
+Disconnect-MgGraph
